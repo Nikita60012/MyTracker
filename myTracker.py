9d67082c873857fd1442ceaa04521c6bf92f19b8
Nikita60012 <96587581+Nikita60012@users.noreply.github.com>
2023-05-10 01:53:43 +0300

    Add files via upload

--- myTracker.py
+++ myTracker.py
@@ -1,7 +1,5 @@
 import cv2
 import numpy as np
-import sys
-import glob
 
 import torch
 import time
@@ -10,15 +8,15 @@ class YoloDetector:
 
     def __init__(self,model_name):
 
-        self.model = self.load_model(model_name)
-        self.classes = self.model.names
-        #print(self.classes)
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model = self.load_model(model_name) # Подгрузка модели
+        self.classes = self.model.names # Список объектов для трекинга
+        print(self.classes)
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu' #Использование видеопамяти, если представляется возможным
         print("Using Device: ", self.device)
 
     def load_model(self, model_name):
 
-        if model_name:
+        if model_name:  #Если вместо None указана какая-то своя модель
             model = torch.hub.load('ultralytics/yolov5', 'custom', path = model_name, force_reload=True)
         else:
             model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained = True)
@@ -26,13 +24,14 @@ class YoloDetector:
 
     def score_frame(self, frame):
 
+# Смена размера кадра
         self.model.to(self.device)
         downscale_factor = 2
         width = int(frame.shape[1]/downscale_factor)
         height = int(frame.shape[0] / downscale_factor)
         frame = cv2.resize(frame, (width,height))
-        #frame = frame.to(self.device)
 
+        #Применение модели к кадру
         results = self.model(frame)
 
         labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
@@ -56,32 +55,51 @@ class YoloDetector:
         for i in range(n):
             row = cord[i]
 
-            if row[4] >= confidence:
+            #Разметка бокса
+            if row[4] >= confidence: #[4]?
                 x1,y1,x2,y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
 
-                if self.class_to_label(labels[i]) == 'person':
+                #Определение класса объекта
 
-                    x_center = x1 + (x2 - x1)
-                    y_center = y1 + ((y2 - y1) / 2)
-
-                    twlh = np.asarray([x1, y1, int(x2-x1), int(y2-y1)], dtype=np.float32)
+                if self.class_to_label(labels[i]) == 'bicycle':
                     confidence = float(row[4].item())
-                    feature = 'person'
+                    detections.append(([x1, y1, int(x2 - x1), int(y2 - y1)], row[4].item(), 'bicycle'))
 
-                    detections.append(([x1, y1, int(x2-x1), int(y2-y1)], row[4].item(), 'person'))
+                if self.class_to_label(labels[i]) == 'car':
+                    confidence = float(row[4].item())
+                    detections.append(([x1, y1, int(x2 - x1), int(y2 - y1)], row[4].item(), 'car'))
 
+                if self.class_to_label(labels[i]) == 'motorcycle':
+                    confidence = float(row[4].item())
+                    detections.append(([x1, y1, int(x2 - x1), int(y2 - y1)], row[4].item(), 'motorcycle'))
+
+                if self.class_to_label(labels[i]) == 'bus':
+                    confidence = float(row[4].item())
+                    detections.append(([x1, y1, int(x2 - x1), int(y2 - y1)], row[4].item(), 'bus'))
+
+                if self.class_to_label(labels[i]) == 'truck':
+                    confidence = float(row[4].item())
+                    detections.append(([x1, y1, int(x2 - x1), int(y2 - y1)], row[4].item(), 'truck'))
+
+                if self.class_to_label(labels[i]) == 'lorry':
+                    confidence = float(row[4].item())
+                    detections.append(([x1, y1, int(x2 - x1), int(y2 - y1)], row[4].item(), 'lorry'))
+
+                if self.class_to_label(labels[i]) == 'trailer':
+                    confidence = float(row[4].item())
+                    detections.append(([x1, y1, int(x2 - x1), int(y2 - y1)], row[4].item(), 'trailer'))
 
         return frame,detections
 
-
-cap = cv2.VideoCapture("D:\\Users\\Никита\\Desktop\\person_test.mp4")
+#Выбор видео и установка размеров окна просмотра
+cap = cv2.VideoCapture("D:\\Users\\Никита\\Desktop\\test4.mp4")
 
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-
-detector = YoloDetector(model_name=None)
+#Создание объекта yolo детектора
+detector = YoloDetector(model_name="radar.pt")
 
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
@@ -107,12 +125,15 @@ while cap.isOpened():
 
     start = time.perf_counter()
 
+    #Отправка кадра на детект
     results = detector.score_frame(img)
+    #Отправка кадра на разметку обводки по координатам
     img, detections = detector.plot_boxes(results, img, height=img.shape[0], width=img.shape[1], confidence=0.5)
 
-
+    #Присвоение id-шника
     tracks = object_tracker.update_tracks(detections, frame=img)
 
+    #Получение id-шника и координат для отрисовки
     for track in tracks:
         if not track.is_confirmed():
             continue
@@ -120,17 +141,18 @@ while cap.isOpened():
         ltrb = track.to_ltrb()
 
         bbox = ltrb
-
+        #отрисовка бокса и id-шника
         cv2.rectangle(img,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0,0,255),2)
         cv2.putText(img, "ID: " + str(track_id), (int(bbox[0]), int(bbox[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
 
-
+    #Подсчёт FPS
     end = time.perf_counter()
     totalTime = end - start
     fps = 1/ totalTime
 
+    #отрисовка FPS
     cv2.putText(img, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
-    cv2.imshow('img', img)
+    cv2.imshow('MyTracker', img)
 
 
     if cv2.waitKey(1) & 0xFF == 27:
